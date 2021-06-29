68326140620eb07ba20338d099392764c9f2f2db
David Bouscarle <d.bouscarle@gmail.com>
2021-06-29 13:40:21 +0200

    Dev app in pending to resolve ce connard de bug...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [longitude, setLongitude] = useState([]);
+  const [latitude, setLatitude] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const apiCallHandler = async () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        }, (error) => {
+          console.warn(error.message)}
+      );
+          
+      await fetch(`${process.env.REACT_APP_API_URL}/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+        .then(response => response.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+    }
+      return () => {
+        apiCallHandler();
+      }
+  }, [latitude, longitude]);
+  
   return (
     <div className="App">
-
+      {console.log(data)}
     </div>
   );
 }
