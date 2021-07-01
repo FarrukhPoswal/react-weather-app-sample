8fdbf5d0cbec802e7b4a0e1a8dcbdc7bae167684
David Bouscarle <d.bouscarle@gmail.com>
2021-07-01 09:53:14 +0200

    Add img/icon URL to the daily card

--- src/Components/WeatherDaily.jsx
+++ src/Components/WeatherDaily.jsx
@@ -7,9 +7,13 @@ const WeatherDaily = ({ datas }) => {
             {datas.daily.map(data => (
                 <DailyCardItem key={data.dt}>
                     <h4>{new Date(data.dt * 1000).toLocaleDateString('fr-FR')}</h4>
-                    <p>Température Maximale : {data.temp.day}</p>
+                    <img src={`${process.env.REACT_APP_ICON_URL}/${data.weather[0].icon}@2x.png`} alt="weather icon" />
+                    <p>Température Maximale : {data.temp.max}</p>
+                    <p>Température Minimale : {data.temp.min}</p>
                     <p>Levé de soleil : {new Date(data.sunrise * 1000).toLocaleTimeString('fr-FR')}</p>
                     <p>Couché de soleil : {new Date(data.sunset * 1000).toLocaleTimeString('fr-FR')}</p>
+                    <p>Levé de lune : {new Date(data.moonrise * 1000).toLocaleTimeString('fr-FR')}</p>
+                    <p>Couché de lune : {new Date(data.moonset * 1000).toLocaleTimeString('fr-FR')}</p>
                 </DailyCardItem>
             ))}
         </DailyCardWrapper>
